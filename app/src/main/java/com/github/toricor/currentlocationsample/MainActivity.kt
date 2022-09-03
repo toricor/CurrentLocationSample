e9ca3f4e7a4d9ab57f546997b5f66d6af8de4efc
toricor <toriyabe@mfac.jp>
2022-09-03 13:22:15 +0900

    initial commit

---
+++ app/src/main/java/com/github/toricor/currentlocationsample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.github.toricor.currentlocationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.toricor.currentlocationsample.ui.theme.CurrentLocationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CurrentLocationSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CurrentLocationSampleTheme {
+        Greeting("Android")
+    }
+}
