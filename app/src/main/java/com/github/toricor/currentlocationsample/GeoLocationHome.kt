b5aa61d09d4d8e572d0c6d2a844827410fa235d5
toricor <toriyabe@mfac.jp>
2022-10-03 23:47:12 +0900

    enable dark background preview

--- app/src/main/java/com/github/toricor/currentlocationsample/GeoLocationHome.kt
+++ app/src/main/java/com/github/toricor/currentlocationsample/GeoLocationHome.kt
@@ -1,15 +1,18 @@
 package com.github.toricor.currentlocationsample
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.toricor.currentlocationsample.ui.theme.CurrentLocationSampleTheme
@@ -116,18 +119,20 @@ fun StatelessGeoLocation(
 @Composable
 fun GeoLocationWithLoadingPreview() {
     CurrentLocationSampleTheme {
-        StatelessGeoLocationWithLoading(
-            locationPayload = LocationPayload(
-                latitude = 35.6809591,
-                longitude = 139.7673068,
-                accuracy = 12.589F,
-                time = 1662194662614,
-                speed = 50.03244F,
-                mocked = false,
-            ),
-            onClick = {},
-            isUpdating = true,
-        )
+        Surface {
+            StatelessGeoLocationWithLoading(
+                locationPayload = LocationPayload(
+                    latitude = 35.6809591,
+                    longitude = 139.7673068,
+                    accuracy = 12.589F,
+                    time = 1662194662614,
+                    speed = 50.03244F,
+                    mocked = false,
+                ),
+                onClick = {},
+                isUpdating = true,
+            )
+        }
     }
 }
 
@@ -138,18 +143,20 @@ fun GeoLocationWithLoadingPreview() {
 @Composable
 fun GeoLocationWithLoadingDarkPreview() {
     CurrentLocationSampleTheme(darkTheme = true) {
-        StatelessGeoLocationWithLoading(
-            locationPayload = LocationPayload(
-                latitude = 35.6809591,
-                longitude = 139.7673068,
-                accuracy = 12.589F,
-                time = 1662194662614,
-                speed = 50.03244F,
-                mocked = false,
-            ),
-            onClick = {},
-            isUpdating = true,
-        )
+        Surface {
+            StatelessGeoLocationWithLoading(
+                locationPayload = LocationPayload(
+                    latitude = 35.6809591,
+                    longitude = 139.7673068,
+                    accuracy = 12.589F,
+                    time = 1662194662614,
+                    speed = 50.03244F,
+                    mocked = false,
+                ),
+                onClick = {},
+                isUpdating = true,
+            )
+        }
     }
 }
 
@@ -160,15 +167,17 @@ fun GeoLocationWithLoadingDarkPreview() {
 @Composable
 fun GeoLocationPreview() {
     CurrentLocationSampleTheme {
-        StatelessGeoLocation(
-            latitude = 35.6809591,
-            longitude = 139.7673068,
-            accuracy = 12.589F,
-            time = 1662194662614,
-            speed = 50.03244F,
-            mocked = false,
-            onClick = {},
-        )
+        Surface {
+            StatelessGeoLocation(
+                latitude = 35.6809591,
+                longitude = 139.7673068,
+                accuracy = 12.589F,
+                time = 1662194662614,
+                speed = 50.03244F,
+                mocked = false,
+                onClick = {},
+            )
+        }
     }
 }
 
@@ -179,14 +188,16 @@ fun GeoLocationPreview() {
 @Composable
 fun GeoLocationDarkPreview() {
     CurrentLocationSampleTheme(darkTheme = true) {
-        StatelessGeoLocation(
-            latitude = 35.6809591,
-            longitude = 139.7673068,
-            accuracy = 12.589F,
-            time = 1662194662614,
-            speed = 50.03244F,
-            mocked = false,
-            onClick = {},
-        )
+        Surface {
+            StatelessGeoLocation(
+                latitude = 35.6809591,
+                longitude = 139.7673068,
+                accuracy = 12.589F,
+                time = 1662194662614,
+                speed = 50.03244F,
+                mocked = false,
+                onClick = {},
+            )
+        }
     }
 }
