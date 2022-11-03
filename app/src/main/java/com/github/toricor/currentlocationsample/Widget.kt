7e9b4b3d6bd9e3e1786db3f43fe55651f2e920c2
toricor <toriyabe@mfac.jp>
2022-11-03 23:09:18 +0900

    can start activity when pressed

--- app/src/main/java/com/github/toricor/currentlocationsample/Widget.kt
+++ app/src/main/java/com/github/toricor/currentlocationsample/Widget.kt
@@ -4,17 +4,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.fillMaxSize
 import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
 
-class HelloWorldWidget: GlanceAppWidget() {
+class HelloWorldWidget : GlanceAppWidget() {
     @Composable
     override fun Content() {
         Text(
             text = "Hello world",
-            modifier = GlanceModifier.background(Color.Gray)
+            modifier = GlanceModifier
+                .clickable(actionStartActivity<MainActivity>())
+                .fillMaxSize()
+                .background(Color(0, 200, 0)),
+            style = TextStyle(textAlign = TextAlign.Center, )
         )
     }
 }
